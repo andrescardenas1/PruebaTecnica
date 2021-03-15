ec3386a3bf2c9ab286313880068893c2979e39d7
andrescardenas1 <cardenas881@hotmail.com>
2021-03-15 12:21:12 -0500

    Prueba Tecnica

---
+++ src/app/core/mocks/mock-users.ts
@@ -0,0 +1,6 @@
+import {User} from "../models/user.model";
+
+
+export const USERS: User[] = [
+  {id: 1, name: 'Andres', surname: 'Cardenas', email: 'cardenas881@hotmail.com', username: 'Dusk', password: 'dusk'},
+];
